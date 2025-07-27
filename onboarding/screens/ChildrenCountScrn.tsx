03a20810e16cfbff48d8e93b5927bd6e82dfbd40
gsarthakdev <sarthak@1700ventures.com>
2025-07-27 17:18:45 -0400

    Add "See privacy policy" button on first onboarding screen #7

--- onboarding/screens/ChildrenCountScrn.tsx
+++ onboarding/screens/ChildrenCountScrn.tsx
@@ -1,46 +1,50 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { Linking, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import OptionButton from '../components/OptionButton'
 import Header from '../components/Header'
 
-export default function ChildrenCountScrn({navigation}: {navigation: any}) {
+export default function ChildrenCountScrn({ navigation }: { navigation: any }) {
+  const PRIVACY_POLICY_URL = "https://docs.google.com/document/d/1xth4Opaqo07Z-7QT4OeBpIXzefdVxJR6dikuO-BzMFk/edit?usp=sharing"
   return (
     <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: "#EAE4CF" }}>
       <Header hideBackButton={true} />
       {/* <Text>How many children are receiving therapy?</Text> */}
-            <View style={{ flex: 1, marginTop: 30}}>
+      <View style={{ flex: 1, marginTop: 30 }}>
 
-              <View style={{ marginLeft: 20, }}>
-                <Text
-                  style={{
-                    color: "#000000",
-                    fontWeight: "bold",
-                    fontSize: 30,
-       
-                  }}
-                >
-                  How many children are receiving therapy?
-                </Text>
-                <Text
-                  style={{
-                    color: "grey",
-                    // fontFamily: "regular",
-                    fontSize: 15,
-                    // marginLeft: 50,
-                    marginTop: 15
-                  }}
-                >
-                  the number of children you want to log data for
-                </Text>
-              </View>
+        <View style={{ marginLeft: 20, }}>
+          <Text
+            style={{
+              color: "#000000",
+              fontWeight: "bold",
+              fontSize: 30,
 
-                <View style={{ alignItems: "center", justifyContent: "center", marginTop: 100  }}>
-                  <OptionButton text="Just one" onPress={() => navigation.push("OneChildScrn")}/>
-                  <OptionButton text="More than one" onPress={() => navigation.push("MultiChildScrn")}/>
-                </View>
+            }}
+          >
+            How many children are receiving therapy?
+          </Text>
+          <Text
+            style={{
+              color: "grey",
+              // fontFamily: "regular",
+              fontSize: 15,
+              // marginLeft: 50,
+              marginTop: 15
+            }}
+          >
+            the number of children you want to log data for
+          </Text>
+        </View>
 
-            </View>
-          {/* </View> */}
+        <View style={{ alignItems: "center", justifyContent: "center", marginTop: 100 }}>
+          <OptionButton text="Just one" onPress={() => navigation.push("OneChildScrn")} />
+          <OptionButton text="More than one" onPress={() => navigation.push("MultiChildScrn")} />
+        </View>
+
+      </View>
+      <TouchableOpacity style={{marginBottom: 20}} onPress={() => Linking.openURL(PRIVACY_POLICY_URL)}>
+        <Text style={{color:"blue"}}>See privacy policy</Text>
+      </TouchableOpacity>
+      {/* </View> */}
     </SafeAreaView>
   )
 }
